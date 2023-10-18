590d21a9e4a7ad4e84ff65198684b5b07904d49d
uyamanaka <ushio.yamanaka@hacomono.co.jp>
2023-10-18 18:31:19 +0900

    fix: types

--- examples/insider-vue2.7/src/views/ContactView.vue
+++ examples/insider-vue2.7/src/views/ContactView.vue
@@ -3,15 +3,3 @@
     /contact
   </span>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  computed: {
-    test(): number {
-      return this.$passerelle.layout.enclosure.window.height
-    }
-  }
-})
-</script>
--- examples/insider-vue3/src/views/ContactView.vue
+++ examples/insider-vue3/src/views/ContactView.vue
@@ -1,15 +1,3 @@
 <template>
   /contact
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  computed: {
-    test(): number {
-      return this.$passerelle.layout.enclosure.window.height
-    }
-  }
-})
-</script>
