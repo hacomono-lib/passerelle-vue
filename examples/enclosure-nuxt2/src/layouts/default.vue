95d391985e08cf756f9eb58a1684230a70394975
uyamanaka <ushio.yamanaka@hacomono.co.jp>
2023-10-23 18:03:06 +0900

    WIP: add example enclosure nuxt2

---
+++ examples/enclosure-nuxt2/src/layouts/default.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { Playground } from 'fixture-enclosure'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Playground,
+  }
+})
+</script>
+
+<template>
+  <div>
+    <section>
+      <Header />
+      <Playground name="passerelle-bridge" />
+    </section>
+
+    <section>
+      <NuxtChild />
+    </section>
+  </div>
+</template>
+
+<style scoped>
+section {
+  line-height: 1.5;
+}
+
+div {
+  height: 95vh;
+  width: inherit;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0 2rem;
+}
+</style>
